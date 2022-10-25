87bf8fbab5491b8af7424d95d20fec9db8d27419
Maia Liamgot <maialiamgotindex@gmail.com>
2022-10-24 23:21:34 -0300

    entregando

---
+++ js/datos.js
@@ -0,0 +1,29 @@
+
+/* let nombreCliente = document.getElementById("nombre");
+localStorage.setItem("nombre", nombreCliente);
+
+let nombre = localStorage.getItem("nombre")
+console.log(nombre);
+ */
+/* document.querySelector("h2").innerHTML = 'Bienvenid@ ${nombre}' */
+
+let nombre = document.getElementById("nombre")
+localStorage.setItem("name", "nombre");
+nombre = localStorage.getItem("nombre")
+
+
+
+
+
+
+
+
+/* .addEventListener("change", () => {
+    if (texto.value == ‘ ’) {
+        texto.style.bakcground = ‘red’;
+        } else{
+        parrafo.innerHTML = texto.value;
+        }
+        })
+        
+}) */
--- js/main.js
+++ js/main.js
@@ -1,41 +1,33 @@
-class Cliente {
-	constructor(nombre, apellido) {
-	this.nombre = nombre;
-	this.apellido = apellido;
-	}
-	}
-	
-	const listaCliente = [];
-	
-	const guardarCliente = () => {
-	let nombre = document.getElementById("nombre").value;
-	let apellido = document.getElementById("apellido").value;
-	
-	nombre.addEventListener("change", ()=>{
-		if (nombre.value == '') {
-		nombre.style.bakcground = 'red';
-		} else{
-		console.log = nombre.value;
+	class Cliente {
+		constructor(nombre, apellido) {
+		this.nombre = nombre;
+		this.apellido = apellido;
 		}
-		})
+		}
+		
+		const listaCliente = [];
+		
+		const guardarCliente = () => {
+		const nombre = document.getElementById("nombre").value;
+		const apellido = document.getElementById("apellido").value;
+		
+		let nuevoCliente = new Cliente (nombre, apellido);
+		listaCliente.push(nuevoCliente);
+		console.log(listaCliente)
+		}
 
-		nombre.onchange = (Cliente) = {}
-
-	let nuevoCliente = new Cliente (nombre, apellido);
-	listaCliente.push(nuevoCliente);
-	console.log(listaCliente)
-	}
 
 	const hamburguesa = document.getElementById("hamburguesa")
 	const bebida = document.getElementById("bebida")
 
 	function Pedido() {
-	const parrafo = document.getElementById("parrafo");
-	parrafo.innerHTML = (`Tu pedido es ${hamburguesa.value} con ${bebida.value}`)
+	const parrafo2 = document.getElementById("parrafo2");
+	parrafo2.innerHTML = (`Tu pedido es ${hamburguesa.value} con ${bebida.value}`)
 	}
 
+
+	localStorage.setItem("pedido", Pedido);
 	
-		
 
 	let hamburguesaStock = ["Mcfiesta", "Mcnifica", "Big Mac"];
    
@@ -43,9 +35,9 @@ class Cliente {
     const parrafo2 = document.getElementById("parrafo2");
     console.log(stock);
     const texto = document.getElementById("texto");
-    if (stock === 'false') {
-        parrafo2.innerHTML = (`no contamos con stock de esa hamburguesa`);
-    } else (`Tu pedido es ${hamburguesa.value} con ${bebida.value}`)
+	if (stock === false) {
+		parrafo2.innerHTML = `no contamos con stock de esa hamburguesa`;
+	} else parrafo2.innerHTML = `Tu pedido es ${hamburguesa.value} con ${bebida.value}`;
 	
 
     
